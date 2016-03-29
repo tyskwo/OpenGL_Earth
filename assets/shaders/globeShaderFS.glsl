d563231aaf550d8723e16a84dffa3c567bf1f1e6
tyler.wood <tyler.wood@localhost>
2016-03-28 23:47:21 -0400

    [LAB1][CHANGED] No visible progress, but clouds and specular are being loaded into the program and shaders.

--- assets/shaders/globeShaderFS.glsl
+++ assets/shaders/globeShaderFS.glsl
@@ -5,8 +5,9 @@
 	in		vec2 v_texCoord;			//the texture coordinate for the vertex
 
 	uniform sampler2D earth_diffuse;	//texture that holds the earth map		
-	uniform sampler2D earth_specular;	//texture that holds the earth's specular
+	//uniform sampler2D earth_specular;	//texture that holds the earth's specular
 	uniform sampler2D earth_night;		//texture that holds the earth map at night
+	//uniform sampler2D earth_clouds;		//texture that holds the earth's clouds
 
 			vec3 color;					//the color of the sphere
 			vec3 vertNorm_interpolated;	//the interpolated normal from each vertex
@@ -17,21 +18,12 @@
 
 void main()
 {
-//get the color based on the two passed in textures.
+//get the color for each texture at the given coordinate
 	vec3 color_diffuse  = texture2D(earth_diffuse,  vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
 	vec3 color_night    = texture2D(earth_night,    vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
-	vec3 color_specular = texture2D(earth_specular, vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
+	//vec3 color_clouds   = texture2D(earth_clouds,   vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
+	//vec3 color_specular = texture2D(earth_specular, vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
 
-	vec3 diffuse_inverse = vec3(1,1,1) - color_diffuse;
-
-	color = color_specular;
-	color = (color * diffuse_inverse) + color_diffuse;
-
-
-	//color = (color_specular - color_diffuse) * when_eq(color_specular.r, 1.0);
-	//color = (color_diffuse) * when_eq(color_specular.r, 0.0);
-
-	//(Earth * illumination + night * (1-illumination)) * (1-cloud_alpha) + cloud_color * illumination * cloud_alpha
 
 //normalize the interpolated normal
 	vertNorm_interpolated = normalize(v_vertNormal);
@@ -41,4 +33,6 @@ void main()
 
 //pass the calculated color to the renderer
 	o_color = (color_diffuse * diffuseMultiplier + color_night * (1.0f - diffuseMultiplier));
+
+	//(Earth * illumination + night * (1-illumination)) * (1-cloud_alpha) + cloud_color * illumination * cloud_alpha
 }

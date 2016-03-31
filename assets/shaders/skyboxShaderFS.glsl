60049776e4663d32ec6624abcc6e7f336dfd5fbe
tyskwo <tyskwo@localhost>
2016-03-31 10:49:40 -0400

    [ADDED] skybox, need to find a higher resolution one. [BROKEN] normal mapping still broken.

--- assets/shaders/skyboxShaderFS.glsl
+++ assets/shaders/skyboxShaderFS.glsl
@@ -1,48 +1,15 @@
 #version 330 core
 
-	in		vec3 v_lightDirection;		//the light direction
-	in		vec2 v_texCoord;			//the texture coordinate for the vertex
+	in		vec2	  v_texCoord;		//the texture coordinate for the vertex
 
-	uniform sampler2D earth_diffuse;	//texture that holds the earth map		
-	uniform sampler2D earth_specular;	//texture that holds the earth's specular
-	uniform sampler2D earth_night;		//texture that holds the earth map at night
-	uniform sampler2D earth_clouds;		//texture that holds the earth's clouds
-	uniform sampler2D earth_clouds_mask;//texture that holds the cloud mask
-	uniform sampler2D earth_normals;	//texture for earth normals
+	uniform sampler2D skybox_diffuse;	//texture that holds the skybox	
 
-			vec3 color;					//the color of the sphere
-			vec3 vertNorm_interpolated;	//the interpolated normal from each vertex
-
-			float shininess = 50;		//specular lighting values
-			float specularIntensity = 2;
-
-	out		vec3 o_color;				//the color to pass to the renderer
+	out		vec3	  o_color;			//the color to pass to the renderer
 
 	
 
 void main()
 {
-//get the color for each texture at the given coordinate
-	vec3 color_diffuse  = texture2D(earth_diffuse,     vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
-	vec3 color_night    = texture2D(earth_night,       vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
-	vec3 color_clouds   = texture2D(earth_clouds,      vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
-	vec3 clouds_mask    = texture2D(earth_clouds_mask, vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
-	vec3 color_specular = texture2D(earth_specular,    vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
-	vec3 color_normals  = texture2D(earth_normals,     vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
-		 color_normals = (color_normals * 2.0) - 1.0;
-
-//normalize the interpolated normal
-	vertNorm_interpolated = normalize(color_normals);
-
-//get the diffuse multiplier
-	float diffuseMultiplier = dot(vertNorm_interpolated, v_lightDirection);
-
-//get the interpolated color based on the diffuse texture, night texture, and clouds texture.
-	color = (color_diffuse * diffuseMultiplier + color_night * (1.0f - diffuseMultiplier) + color_clouds * (1.0f - clouds_mask) * (diffuseMultiplier));
-
-//get the specular value based on the specular intensity and specular mask.
-	vec3 specular = specularIntensity * color_specular * max(pow(max(diffuseMultiplier, 0), shininess), 0) * length(color);
-	
-//pass the calculated color to the renderer; combine the specular with the interpolated color
-	o_color = color + specular;
+//get the color from the texture at the given coordinate
+	o_color  = texture2D(skybox_diffuse, vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
 }
--- assets/shaders/skyboxShaderVS.glsl
+++ assets/shaders/skyboxShaderVS.glsl
@@ -6,22 +6,13 @@
 
 	uniform mat4 u_vp;				//the view projection matrix
 	uniform mat4 u_model;			//the model
-	uniform vec3 u_lightPosition;	//the light position
 
-	out		vec3 v_lightDirection;	//the light direction
 	out		vec2 v_texCoord;		//the vertex texture coordinate
 
 void main()
 { 
 //determine the position of the vertex
 	gl_Position = u_vp * u_model * vec4(a_vertPos, 1);
-
-//transform into world view
-	vec3 vertexWorldPosition = (u_model * vec4(a_vertPos, 1)).xyz;
-
-//determine the light direction based on world view
-	vec3 lightDirection = u_lightPosition - vertexWorldPosition;
-	v_lightDirection	= normalize(lightDirection);
 
 //pass the texture coordinate through to the fragment shader
 	v_texCoord = a_vertTexCoord0;

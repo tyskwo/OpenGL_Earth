55ca25ef41f0341b0dce1a3a7688272c8047c42c
Eric <Eric@localhost>
2016-04-03 19:25:06 -0400

    [CHANGED] Cloud Texture

--- assets/shaders/globeShaderFS.glsl
+++ assets/shaders/globeShaderFS.glsl
@@ -8,7 +8,7 @@
 	uniform sampler2D earth_specular;			//texture that holds the earth's specular
 	uniform sampler2D earth_night;				//texture that holds the earth map at night
 	uniform sampler2D earth_clouds;				//texture that holds the earth's clouds
-	uniform sampler2D earth_clouds_mask;		//texture that holds the cloud mask
+	//uniform sampler2D earth_clouds_mask;		//texture that holds the cloud mask
 	uniform sampler2D earth_normals;			//texture for earth normals
 	uniform float	  u_cloud_shift;			//amount to Shift clouds by
 
@@ -28,7 +28,7 @@ void main()
 	vec3 color_diffuse  = texture2D(earth_diffuse,     vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
 	vec3 color_night    = texture2D(earth_night,       vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
 	vec3 color_clouds   = texture2D(earth_clouds,      vec2(v_texCoord.s + u_cloud_shift, 1 - v_texCoord.t)).rgb;
-	vec3 clouds_mask    = texture2D(earth_clouds_mask, vec2(v_texCoord.s + u_cloud_shift, 1 - v_texCoord.t)).rgb;
+	//vec3 clouds_mask    = texture2D(earth_clouds_mask, vec2(v_texCoord.s + u_cloud_shift, 1 - v_texCoord.t)).rgb;
 	vec3 color_specular = texture2D(earth_specular,    vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
 	vec3 color_normals  = texture2D(earth_normals,     vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
 		 color_normals  = (color_normals * 2.0) - 1.0;
@@ -40,7 +40,7 @@ void main()
 	float diffuseMultiplier = dot(vertNorm_interpolated, v_lightDirection);
 
 //get the interpolated color based on the diffuse texture, night texture, and clouds texture.
-	color = (color_diffuse * diffuseMultiplier + color_night * (1.0f - diffuseMultiplier) + color_clouds * (1.0f - clouds_mask) * (diffuseMultiplier));
+	color = (color_diffuse * diffuseMultiplier + color_night * (1.0f - diffuseMultiplier) + color_clouds * (diffuseMultiplier));// + color_clouds * (1.0f - clouds_mask) * (diffuseMultiplier));
 
 //get the specular value based on the specular intensity and specular mask.
 	vec3 specular = specularIntensity * color_specular * max(pow(max(diffuseMultiplier, 0), shininess), 0) * length(color);

262692016c42c151fbc4665f073f4afbf1e253fc
eric <eric@localhost>
2016-04-05 23:52:56 -0400

    [UPDATED] Skybox [REMOVED] Unecessary files

--- assets/shaders/skyboxShaderFS.glsl
+++ assets/shaders/skyboxShaderFS.glsl
@@ -1,21 +1,45 @@
 #version 330 core
 
-	in		vec2	  v_texCoord;		//the texture coordinate for the vertex
+	in		vec2	  v_position;		//the texture coordinate for the vertex
+	in		mat3	  v_vertTBN;		//the vertex tbn matrix
 
-	uniform sampler2D skybox_diffuse;	//texture that holds the skybox	
+	uniform	float	  u_starThreshold;	//the threshold for the stars - anything above this value will be made a star
 
 			vec3	  color;			//intermediary color
+			vec2	  position;			//intermediary position
+			float	  starValue;		//the value of the noise field at the given coordinate
 
 	out		vec3	  o_color;			//the color to pass to the renderer
 
 	
+//randomize a number (found online)
+float hash(float n)
+{
+	return fract((1.0 + cos(n)) * 415.92653);
+}
+
+//generate random noise at the given coordinate
+float noise(in vec2 position)
+{
+    float xhash = hash(position.x * 37.0);
+    float yhash = hash(position.y * 57.0);
+
+    return fract(xhash + yhash);
+}
 
 void main()
 {
-//get the color from the texture at the given coordinate
-	color  = texture2D(skybox_diffuse, vec2(v_texCoord.s, 1 - v_texCoord.t)).rgb;
+//base space-like color
+    color = vec3(0.1, 0.1, 0.13);
 
+    position = v_position + floor(v_vertTBN[0].xy);
+	
+//get the value of the noise field at the coordinate, and test to see if it is above the given threshold
+    starValue = noise(position);
+    starValue = pow((max(starValue - u_starThreshold, 0.0)) / (1.0 - u_starThreshold), 6.0);
 
+//add the value to the color
+	color += vec3(starValue);
 
 //pass the color to the renderer
 	o_color = color;
--- src/tlocVersionCheck.cpp
+++
@@ -1,2 +0,0 @@
-#define TLOC_VERSION_0_2_15056_1459794285
-#include "../tlocVersion.h"

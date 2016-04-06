7ad71bb9f3890b1aa18836510cb1f5b7721dfb4a
tyskwo <tyskwo@users.noreply.github.com>
2016-04-06 19:13:37 -0400

    [EDITED] skybox shader, best twinkling yet, but not perfect.

--- assets/shaders/skyboxShaderFS.glsl
+++ assets/shaders/skyboxShaderFS.glsl
@@ -2,6 +2,7 @@
 
 	in		vec2	  v_position;		//the texture coordinate for the vertex
 	in		vec2	  v_screenPosition;	//the screen coordinate of the vertex
+	in		mat3	  v_vertTBN;		//the tbn matric of the vertex
 
 	uniform sampler2D skybox_diffuse;	//texture that holds the skybox	
 	uniform float     u_twinkleTime;	//current time to alter the twinkle
@@ -10,15 +11,20 @@
 
 	out		vec3	  o_color;			//the color to pass to the renderer
 
-	
+
+//random hash table. http://stackoverflow.com/questions/12964279/whats-the-origin-of-this-glsl-rand-one-liner
+float random(vec2 value)
+{
+	return fract(sin(dot(value.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
 
 void main()
 {
 //get the color from the texture at the given coordinate
 	color = texture2D(skybox_diffuse, vec2(v_position.s, 1 - v_position.t)).rgb;
 
-//twinkle the stars based on their position
-	color *= abs(cos(u_twinkleTime + v_screenPosition.x));
+//twinkle the stars based on their position														 V just to get rid of the warning of unused uniform.
+	color *= abs(cos(u_twinkleTime + random(v_screenPosition.xx) + random(v_screenPosition.yy) + random(v_vertTBN[2].zx)));
 
 //pass the color to the renderer
 	o_color = color;

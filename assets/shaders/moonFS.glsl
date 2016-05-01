b080c73d34ab5bc540bc863fa3bf64fb13a1e84b
tyskwo <tyskwo@localhost>
2016-05-01 15:21:34 -0400

    [FIXED] moon shader.

--- assets/shaders/moonFS.glsl
+++ assets/shaders/moonFS.glsl
@@ -4,21 +4,21 @@
 	in		vec3	  v_vertNormal;				//the vertext normal
 	in		vec2	  v_texCoord;				//the texture coordinate for the vertex
 
-	uniform sampler2D moon_diffuse;			//texture that holds the earth map		
-	uniform sampler2D moon_normals;			//texture for earth normals
+	uniform sampler2D moon_diffuse;				//texture that holds the earth map		
+	uniform sampler2D moon_normals;				//texture for earth normals
 
 			vec4	  color;					//the color of the sphere
 			vec3	  vertNorm_interpolated;	//the interpolated normal from each vertex
 
-	layout (location = 0) out		vec4	   o_color;					//the color to pass to the renderer
+	out		vec4	   o_color;					//the color to pass to the renderer
 
 	
 
 void main()
 {
 //get the color for each texture at the given coordinate
-	vec4 color_diffuse  = texture2D(moon_diffuse,     vec2(v_texCoord.s,				 1 -v_texCoord.t));
-	vec4 color_normals  = texture2D(moon_normals,     vec2(v_texCoord.s,				 1 - v_texCoord.t));
+	vec4 color_diffuse  = texture2D(moon_diffuse, vec2(v_texCoord.s, 1 -v_texCoord.t));
+	vec4 color_normals  = texture2D(moon_normals, vec2(v_texCoord.s, 1 - v_texCoord.t));
 		 color_normals  = (color_normals * 2.0) - 1.0;
 
 //normalize the interpolated normal
@@ -26,10 +26,10 @@ void main()
 
 //get the diffuse and specular multipliers
 	float diffuseMultiplier  = dot(vertNorm_interpolated * color_normals.rgb, v_lightDirection);
-	float specularMultiplier = dot(vertNorm_interpolated,				  v_lightDirection);
+	//float specularMultiplier = dot(vertNorm_interpolated,				      v_lightDirection);
 
 //get the interpolated color based on the diffuse texture
-	color = color_diffuse * diffuseMultiplier;
+	o_color = color_diffuse * diffuseMultiplier;
 
 //gamma correction
 	o_color.r = pow(o_color.r, 1.0 / 2.2);
